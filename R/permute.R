61daf647e75bbc62460e84a90a705382e481e3ba
bobbyhyde <bobbyhyde89@gmail.com>
2021-07-21 12:18:04 +0100

    Headings

--- R/permute.R
+++ R/permute.R
@@ -1,4 +1,18 @@
-permute <- function(data, permute, times, repeats){
+#' Permute
+#'
+#' Permutes a specified outcome a given number of times, and repeats this several times.
+#'
+#' @data a dataframe containing an outcome variable to be permuted
+#' @permute an outcome variable to be permuted
+#' @times the number of times to repeat the permutation
+#' @repeats the number of repeats of this process
+#' @import dplyr
+#' @importFrom purrr rerun
+#' @importFrom rsample permutations
+
+permute <-
+function(data, permute, times, repeats){
   purrr::rerun(repeats, rsample::permutations(data = data, permute = permute, times = times)) %>%
     bind_rows(.id="repeat")
 }
+
