75403caa559029e9b7499544dbc43be802974fd7
bobbyhyde <bobbyhyde89@gmail.com>
2021-07-21 15:14:05 +0100

    model_mbic returns df of included variables

---
+++ R/model_mbic.R
@@ -0,0 +1,37 @@
+#' model_mbic
+#'
+#' Function to model mbic selection process on a given dataframe
+#'
+#' @data a dataframe containing an outcome variable to be permuted (usually coming from nested bootstrap data)
+#' @outcome the outcome as a string (i.e. "y")
+#' @minpv the minpv value for initial filtering
+#'
+#' @import bigstep
+#' @import dplyr
+#'
+
+model_mbic <- function(data, outcome, minpv){
+
+  data <- data %>%
+    as.data.frame()
+
+  y_temp <-data %>%
+    select(all_of(outcome)) %>%
+    as.matrix()
+
+  x_temp <- data %>%
+    select(-all_of(outcome))
+
+  mBIC_gwas <- bigstep::prepare_data(y_temp, x_temp, verbose = FALSE)
+
+  coef_mbic <- mBIC_gwas %>%
+    reduce_matrix(minpv = minpv) %>%
+    fast_forward(crit=mbic) %>%
+    multi_backward(crit=mbic) %>%
+    summary() %>%
+    coef() %>%
+    as.data.frame() %>%
+    rownames_to_column(., var = "variable")
+
+  coef_mbic
+}
