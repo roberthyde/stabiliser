c4776d5a69d22ebbf30cc17da14d804339aebd90
bobbyhyde <bobbyhyde89@gmail.com>
2021-07-21 21:31:53 +0100

    Calculate permutation threshold for null model

--- R/permute.R
+++ R/permute.R
@@ -1,6 +1,6 @@
 #' Permute
 #'
-#' Permutes a specified outcome a given number of times, and repeats this several times.
+#' Calculates permutation threshold for null model, where a specified model is run over multiple bootstrap resamples of multiple permuted version of the dataset.
 #'
 #' @data a dataframe containing an outcome variable to be permuted
 #' @outcome the outcome to be permuted as a string (i.e. "y")
@@ -9,14 +9,14 @@
 #' @model the model to be used (i.e. model_mbic)
 #'
 #' @import dplyr
-#' @importFrom purrr rerun
+#' @import purrr
 #' @importFrom rsample permutations
 #'
 
 permute <- function(data, outcome, permutations, boot_reps, model, ...){
   rsample::permutations(data = data, permute = outcome, times = permutations) %>%
-    mutate(stabs = map(.x = .$splits, .f = ~as.data.frame(.) %>% boot_model(., outcome=outcome, boot_reps = boot_reps, model=model, minpv=minpv))) %>%
-    select(id, stabs)
+    mutate(stab_df = map(.x = .$splits, .f = ~as.data.frame(.) %>% boot_model(., outcome=outcome, boot_reps = boot_reps, model=model, minpv=minpv)),
+           perm_thresh = map(stab_df, ~as_vector(.x$stability) %>% ecdf() %>% quantile(., probs=1))) %>%
+    unnest(perm_thresh) %>%
+    summarise(mean_perm_thresh = mean(perm_thresh))
 }
-
-test <- permute(data = sim_dat, outcome="y", permutations=5, boot_reps=2, model=model_mbic, minpv=0.05)
