30d943683ad910a95a8a6e97f7a687abe4ca8264
bobbyhyde <bobbyhyde89@gmail.com>
2021-07-21 12:17:55 +0100

    Initial commit

---
+++ R/stabiliser.R
@@ -0,0 +1,10 @@
+#' stabiliser: Robust variable selection using stability selection
+#'
+#' This package uses bootstrap resampling and an objective selection stability threshold to provide a robust method of selecting variables truly associated with an outcome.
+#'
+#' @author Robert Hyde \email{robert.hyde1@nottingham.ac.uk}
+#' @author Martin Green
+#' @author Eliana Lima
+#' @docType package
+#' @name stabiliser
+"_PACKAGE"
