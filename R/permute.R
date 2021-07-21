a163a3a5d628990f274eeb626479897273b91889
bobbyhyde <bobbyhyde89@gmail.com>
2021-07-21 21:19:32 +0100

    Functions with bootstrapping nested within each permutation

--- R/permute.R
+++ R/permute.R
@@ -3,16 +3,20 @@
 #' Permutes a specified outcome a given number of times, and repeats this several times.
 #'
 #' @data a dataframe containing an outcome variable to be permuted
-#' @permute an outcome variable to be permuted
-#' @times the number of times to repeat the permutation
-#' @repeats the number of repeats of this process
+#' @outcome the outcome to be permuted as a string (i.e. "y")
+#' @permutations the number of times to be permuted per repeat
+#' @repeats the number of times to repeat each set of permutations
+#' @model the model to be used (i.e. model_mbic)
+#'
 #' @import dplyr
 #' @importFrom purrr rerun
 #' @importFrom rsample permutations
+#'
 
-permute <-
-function(data, permute, times, repeats){
-  purrr::rerun(repeats, rsample::permutations(data = data, permute = permute, times = times)) %>%
-    bind_rows(.id="repeat")
+permute <- function(data, outcome, permutations, boot_reps, model, ...){
+  rsample::permutations(data = data, permute = outcome, times = permutations) %>%
+    mutate(stabs = map(.x = .$splits, .f = ~as.data.frame(.) %>% boot_model(., outcome=outcome, boot_reps = boot_reps, model=model, minpv=minpv))) %>%
+    select(id, stabs)
 }
 
+test <- permute(data = sim_dat, outcome="y", permutations=5, boot_reps=2, model=model_mbic, minpv=0.05)
