f3f05910f4e454f8b9c0da3a25ff46b1bbae0bb4
bobbyhyde <bobbyhyde89@gmail.com>
2021-07-21 12:19:11 +0100

    Headings, rsample::bootstrap

--- R/boot.R
+++ R/boot.R
@@ -1,4 +1,14 @@
+#' Boot
+#'
+#' Performs bootstrap resmampling on a dataframe.
+#'
+#' @data a dataframe containing an outcome variable to be permuted
+#' @repeats the number of bootstrap repeats
+#'
+#' @importFrom rsample bootstraps
+#' @export
+
 boot <-
-function(data){
-  sample_frac(data, 1, replace = TRUE)
+function(data, repeats){
+  rsample::bootstraps(data, repeats)
 }
