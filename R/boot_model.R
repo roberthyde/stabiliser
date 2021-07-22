20761aa8a3644baf7c6f948e4bbae9a5d0b745a2
bobbyhyde <bobbyhyde89@gmail.com>
2021-07-22 11:34:26 +0100

    Add confidence intervals and boot_p

--- R/boot_model.R
+++ R/boot_model.R
@@ -13,14 +13,20 @@
 #' @importFrom utils globalVariables
 #' @importFrom tidyr replace_na
 #'
-#'
 utils::globalVariables(c(".", "variable", "stability"))
 
 boot_model <- function(data, outcome, boot_reps) {
   rsample::bootstraps(data, boot_reps) %>%
     map_df(.x = .$splits, .f = ~ model_mbic(., outcome = outcome)) %>%
     group_by(variable) %>%
-    summarise(stability = (n() / boot_reps) * 100) %>%
+    summarise(mean_coefficient = mean(estimate, na.rm=TRUE),
+              ci_lower = quantile(estimate, 0.025, na.rm = TRUE),
+              ci_upper = quantile(estimate, 0.975, na.rm = TRUE),
+              prop_one_side = case_when(mean_coefficient > 0 ~ length(estimate[estimate>0]),
+                                  mean_coefficient <0 ~ length(estimate[estimate<0])),
+              bootstrap_p = 1-(prop_one_side/length(estimate)),
+              stability = (n() / boot_reps) * 100) %>%
+    select(-prop_one_side) %>%
     right_join(tibble(variable = colnames(data))) %>%
     replace_na(list(stability = 0)) %>%
     arrange(desc(stability))
