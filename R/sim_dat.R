afad9eeb6422203b260ee1151a0e484797df0069
bobbyhyde <bobbyhyde89@gmail.com>
2021-07-22 10:00:32 +0100

    Description of datafile

---
+++ R/sim_dat.R
@@ -0,0 +1,14 @@
+#' Simulated data
+#'
+#' A simulated dataset
+#'
+#' @format A data frame with 100 rows and 1001 variables:
+#'
+"sim_dat"
+
+# \item{x variables}{variables simulated to have a relationship with y}
+# \item{v variables}{variables simulated to have no relationship with y}
+# \describe{
+#   \item{y}{the outcome variable}
+#   ...
+# }
