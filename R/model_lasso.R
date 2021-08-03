775ea54c4dc3697f06c74052c108269ea5a2d9d4
bobbyhyde <bobbyhyde89@gmail.com>
2021-08-03 11:29:00 +0100

    lasso through glmnet directly

--- R/model_lasso.R
+++ R/model_lasso.R
@@ -21,47 +21,47 @@
 utils::globalVariables(c(".", "variable", "estimate", "value"))
 
 model_lasso <- function(data, outcome) {
-  ctrl <- caret::trainControl(
-    method = "repeatedcv",
-    number = 5,
-    repeats = 5
-  )
+  #ctrl <- caret::trainControl(
+  #  method = "repeatedcv",
+  #  number = 5,
+  #  repeats = 5
+  #)#
 
-  data <- data %>%
-    as.data.frame()
+  #data <- data %>%
+  #  as.data.frame()
 
-  fit_lasso <- data %>%
-    caret::train(y ~ .,
-      data = .,
-      trControl = ctrl,
-      method = "glmnet",
-      tuneGrid = expand.grid(alpha = 1, lambda = 0)
-    ) # TODO Currently lambda fixed at zero
+  #fit_lasso <- data %>%
+  #  caret::train(y ~ .,
+  #    data = .,
+  #    trControl = ctrl,
+  #    method = "glmnet",
+  #    tuneGrid = expand.grid(alpha = 1, lambda = 0)
+  #  ) # TODO Currently lambda fixed at zero
 
-  coef(fit_lasso$finalModel, fit_lasso$bestTune$lambda) %>%
-    broom::tidy() %>%
-    rename(
-      variable = row,
-      estimate = value
-    ) %>%
-    filter(variable != "(Intercept)") %>%
-    select(variable, estimate)
-
-  # Or use glmnet directly
-  # y_temp <- data %>%
-  #  select(all_of(outcome)) %>%
-  #  as.matrix()
-
-  # x_temp <- data %>%
-  #  select(-all_of(outcome)) %>%
-  #  as.matrix()
-
-  # fit_lasso <- cv.glmnet(x=x_temp, y=y_temp, alpha = 1, type.measure = "mae", nfolds = 5)
-
-  # coef(fit_lasso, s = "lambda.min") %>%
+  #coef(fit_lasso$finalModel, fit_lasso$bestTune$lambda) %>%
   #  broom::tidy() %>%
-  #  rename(variable = row,
-  #         estimate = value) %>%
+  #  rename(
+  #    variable = row,
+  #    estimate = value
+  #  ) %>%
   #  filter(variable != "(Intercept)") %>%
   #  select(variable, estimate)
+
+  # Or use glmnet directly
+   y_temp <- data %>%
+    select(all_of(outcome)) %>%
+    as.matrix()
+
+   x_temp <- data %>%
+    select(-all_of(outcome)) %>%
+    as.matrix()
+
+   fit_lasso <- cv.glmnet(x=x_temp, y=y_temp, alpha = 1, type.measure = "mae", nfolds = 5)
+
+   coef(fit_lasso, s = "lambda.min") %>%
+    broom::tidy() %>%
+    rename(variable = row,
+           estimate = value) %>%
+    filter(variable != "(Intercept)") %>%
+    select(variable, estimate)
 }
