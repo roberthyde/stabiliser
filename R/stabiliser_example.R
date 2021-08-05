073a72f1f184fd0aa140551c6d98def3d71e1c2c
bobbyhyde <bobbyhyde89@gmail.com>
2021-08-05 21:35:06 +0100

    stabiliser_example documentation

---
+++ R/stabiliser_example.R
@@ -0,0 +1,18 @@
+#' stabiliser_example
+#'
+#' A simulated dataset
+#'
+#' @format A data frame with 50 rows and 100 variables.
+#' The stabiliser_example dataset is a simulated example with the following properties:
+#' 1 simulated outcome variable: `y`
+#' 4 variables simulated to be associated with `y`: `causal1`, `causal2`...
+#' 95 variables simulated to have no association with `y`: `junk1`, `junk2`...
+#'
+"stabiliser_example"
+
+# \item{causal variables}{variables simulated to have a relationship with y}
+# \item{junk variables}{variables simulated to have no relationship with y}
+# \describe{
+#   \item{y}{the outcome variable}
+#   ...
+# }
