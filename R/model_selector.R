d13fb627c1148ad806092fe009d3109fc1e31878
bobbyhyde <bobbyhyde89@gmail.com>
2021-08-05 16:53:01 +0100

    enet added

--- R/model_selector.R
+++ R/model_selector.R
@@ -8,7 +8,9 @@
 utils::globalVariables(c("selected_model"))
 
 model_selector <- function(selected_model) {
-  if (selected_model == "lasso") {
+  if (selected_model == "enet") {
+    selcted_model <- model_enet
+  } else if (selected_model == "lasso") {
     selcted_model <- model_lasso
   } else if (selected_model == "mbic") {
     selected_model <- model_mbic
