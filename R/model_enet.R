7d93264de909308945a6a3b9563d15e5c9274ba4
bobbyhyde <bobbyhyde89@gmail.com>
2021-08-05 16:52:06 +0100

    enet first commit

---
+++ R/model_enet.R
@@ -0,0 +1,67 @@
+#' model_enet
+#'
+#' @name model_enet
+#'
+#' @description Function to model elastic net selection process on a given dataframe
+#'
+#' @param data a dataframe containing an outcome variable to be permuted (usually coming from nested bootstrap data)
+#' @param outcome the outcome as a string (i.e. "y")
+#'
+#' @import glmnet
+#' @import dplyr
+#' @import broom
+#' @importFrom caret train
+#' @importFrom caret trainControl
+#' @importFrom tibble rownames_to_column
+#' @importFrom stats coef
+#' @importFrom utils globalVariables
+#' @importFrom stringr str_remove_all
+#'
+#'
+utils::globalVariables(c(".", "variable", "estimate", "value"))
+
+model_enet <- function(data, outcome) {
+  #ctrl <- caret::trainControl(
+  #  method = "repeatedcv",
+  #  number = 5,
+  #  repeats = 5
+  #)#
+
+  #data <- data %>%
+  #  as.data.frame()
+
+  #fit_lasso <- data %>%
+  #  caret::train(y ~ .,
+  #    data = .,
+  #    trControl = ctrl,
+  #    method = "glmnet",
+  #    tuneGrid = expand.grid(alpha = 1, lambda = 0)
+  #  ) # TODO Currently lambda fixed at zero
+
+  #coef(fit_lasso$finalModel, fit_lasso$bestTune$lambda) %>%
+  #  broom::tidy() %>%
+  #  rename(
+  #    variable = row,
+  #    estimate = value
+  #  ) %>%
+  #  filter(variable != "(Intercept)") %>%
+  #  select(variable, estimate)
+
+  # Or use glmnet directly
+  y_temp <- data %>%
+    select(all_of(outcome)) %>%
+    as.matrix()
+
+  x_temp <- data %>%
+    select(-all_of(outcome)) %>%
+    as.matrix()
+
+  fit_lasso <- cv.glmnet(x=x_temp, y=y_temp, type.measure = "mae", nfolds = 10)
+
+  coef(fit_lasso, s = "lambda.min") %>%
+    broom::tidy() %>%
+    rename(variable = row,
+           estimate = value) %>%
+    filter(variable != "(Intercept)") %>%
+    select(variable, estimate)
+}
