b4473258414537455ededf892d2bda082809ac83
bobbyhyde <bobbyhyde89@gmail.com>
2021-08-05 16:51:55 +0100

    Creating stabiliser_example

--- R/create_sim_dat.R
+++ R/create_sim_dat.R
@@ -27,3 +27,25 @@
 
 # Add data to package
 # use_data(sim_dat)
+
+#Creating stabiliser_example
+#stabiliser_example <- read_csv("D0B_4_TRUE_corr_10_sep_each_at_0.3_R26.csv") %>%
+#  rename(y = y_out) %>%
+#  select(1:104) %>%
+#  select(-contains("X_out"), X_out5, X_out8, X_out4, X_out1) %>%
+#  head(50) %>%
+#  rename(causal1 = X_out5,
+#         causal2 = X_out8,
+#         causal3 = X_out4,
+#         causal4 = X_out1)
+
+#old_names <- stabiliser_example %>%
+#  select(contains("XX")) %>%
+#  colnames()
+
+#new_names <- map_chr(.x = 1:95, .f = ~paste0("junk", .x))
+
+#stabiliser_example <- stabiliser_example %>%
+#  rename_at(.vars = old_names, .funs = ~ new_names) %>%
+#  select(y, contains("causal"), everything())
+#usethis::use_data(stabiliser_example)
