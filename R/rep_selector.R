5f526f9e9d25cf3a261f82af0d2970c59f0a54e6
bobbyhyde <bobbyhyde89@gmail.com>
2021-08-06 13:03:55 +0100

    Automatically determine boot_reps and permutations

---
+++ R/rep_selector.R
@@ -0,0 +1,45 @@
+#' rep_selector_boot
+#'
+#' @name rep_selector_boot
+#'
+#' @description wrapper function to determine the number of bootstrap repeats
+#'
+#' @param data the dataset to analyse.
+#' @param boot_reps the number of bootstrap samples
+#'
+#' @import dplyr
+#'
+
+rep_selector_boot <- function(data, boot_reps){
+  if(boot_reps == "auto"){
+    if(nrow(data) < 200){
+      200
+      }else{
+        100
+      }
+  }else{
+      boot_reps
+    }
+}
+
+#' rep_selector_boot
+#'
+#' @name rep_selector_perm
+#'
+#' @description wrapper function to determine the number of permutations
+#'
+#' @param data the dataset to analyse.
+#' @param permutations the number of times to be permuted per repeat
+#'
+
+rep_selector_perm <- function(data, permutations){
+  if(permutations == "auto"){
+    if(nrow(data) < 200){
+      10
+    }else{
+      5
+    }
+  }else{
+    permutations
+  }
+}
