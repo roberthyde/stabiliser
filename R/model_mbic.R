252a74bef41360e548a4b19bb5db191820b941ac
bobbyhyde <bobbyhyde89@gmail.com>
2021-08-09 14:44:03 +0100

    Remove export

--- R/model_mbic.R
+++ R/model_mbic.R
@@ -14,8 +14,6 @@
 #' @importFrom utils globalVariables
 #' @importFrom stringr str_remove_all
 #'
-#' @export
-#'
 utils::globalVariables(c(".", "variable", "Estimate"))
 
 model_mbic <- function(data, outcome) {
