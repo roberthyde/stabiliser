b3a43faad9fce2d67bb00f8b79da27eeda3f106d
bobbyhyde <bobbyhyde89@gmail.com>
2021-12-23 12:12:35 +0000

    Contact

--- R/stabiliser.R
+++ R/stabiliser.R
@@ -2,7 +2,7 @@
 #'
 #' This package uses bootstrap resampling and an objective selection stability threshold to provide a robust method of selecting variables truly associated with an outcome.
 #'
-#' @author Robert Hyde \email{robert.hyde1@nottingham.ac.uk}
+#' @author Robert Hyde \email{robert.hyde4@nottingham.ac.uk}
 #' @author Martin Green
 #' @author Eliana Lima
 #' @docType package
