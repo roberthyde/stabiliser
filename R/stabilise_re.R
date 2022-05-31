c2aba8771d1ea93318200d749aaf7288cdbfdbe9
bobbyhyde <bobbyhyde89@gmail.com>
2022-05-31 11:50:24 +0100

    Remove models, type and quantile from stabilise_re()

--- R/stabilise_re.R
+++ R/stabilise_re.R
@@ -11,9 +11,6 @@
 #' @param boot_reps The number of bootstrap samples. Default is "auto" which selects number based on dataframe size.
 #' @param permutations The number of times to be permuted per repeat. Default is "auto" which selects number based on dataframe size.
 #' @param perm_boot_reps The number of times to repeat each set of permutations. Default is 5.
-#' @param models The models to select for stabilising. Default is elastic net (models = c("enet")), other available models include "lasso", "mbic", "mcp".
-#' @param type The type of model, either "linear" or "logistic"
-#' @param quantile The quantile of null stabilities to use as a threshold.
 #' @param normalise Normalise numeric variables (TRUE/FALSE)
 #' @param dummy Create dummy variables for factors/characters (TRUE/FALSE)
 #' @param impute Impute missing data (TRUE/FALSE)
@@ -39,8 +36,7 @@ utils::globalVariables(c("models", "in_model", "mean_coefficient", "ci_lower", "
 
 stabilise_re <- function(data, outcome, level_2_id, n_top_filter = 50,
                          boot_reps = "auto", permutations = "auto", perm_boot_reps = 20,
-                         models = c("enet"), type = "linear",
-                         quantile = 1, normalise = TRUE, dummy = TRUE, impute = TRUE) {
+                         normalise = TRUE, dummy = TRUE, impute = TRUE) {
 
   boot_reps <- rep_selector_boot(data = data, boot_reps = boot_reps)
   permutations <- rep_selector_perm(data = data, permutations = permutations)
