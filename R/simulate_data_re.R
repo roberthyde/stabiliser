7e9cd148807e123e50357d06d57f070fb9706a3c
bobbyhyde <bobbyhyde89@gmail.com>
2022-05-31 08:42:21 +0100

    simulate_data_re fixed examples

--- R/simulate_data_re.R
+++ R/simulate_data_re.R
@@ -2,12 +2,10 @@
 #'
 #' @name simulate_data_re
 #'
-#' @description Simulate a dataset with random effects. This can optionally include variables with a given associated with the outcome.
+#' @description Simulate a 500x500 dataset with random effects. This can optionally include variables with a given associated with the outcome.
 #'
-#' @param nrows The number of rows to simulate.
-#' @param ncols The number of columns to simulate.
-#' @param n_true The number of variables truly associated with the outcome.
-#' @param amplitude The strength of association between true variables and the outcome.
+#' @param sd_level_1 Standard deviation of level 1 variables
+#' @param sd_level_2 Standard deviation of level 2 variables
 #'
 #' @return A simulated dataset
 #'
@@ -16,20 +14,31 @@
 #'
 #' @export
 
-simulate_data_re <- function(nrows, ncols, n_true = 2, amplitude = 2,
-                          n_level_2 = 2, sd_level_1 = 0.5, sd_level_2 = 0) {
-  # nlev2 must be positive integer
-  # n_true must be positive integer
+simulate_data_re <- function(sd_level_1 = 2, sd_level_2 = 2) {
+
+  if (sd_level_1 < 0.5 | sd_level_1 > 6 | sd_level_2 < 0.5 | sd_level_2 > 6){
+    stop("For data simulation with random effects, both sd_level_1 and sd_level_2 must be set between 0.5-6.")
+  }
+
+  # TODO: User editable function
+  nrows = 500
+  ncols = 500
+  n_true = 8
+  amplitude = 2
+  n_level_2 = 5
 
   nlev2 <- nrows / n_level_2
+  num_noise_vars = nrows - n_true
 
-  uncor_sd_true <- data.frame(replicate(n_true, rnorm(nrows, 0, 1)))
+  ## FIXED EFFECTS AT LEVEL 1
+  fixed_effect_level_1 <- data.frame(replicate(n_true / 2, rnorm(nrows, 0, 1)))
 
-  ##  FIXED EFFECTS AT LEVEL 2
-  fixed_effect_level_2 <- data.frame(replicate(n_level_2, rep(rnorm(nlev2, 0, 1), each = n_level_2))) # gives a RE to each level 2
+  ## FIXED EFFECTS AT LEVEL 2
+  fixed_effect_level_2 <- data.frame(replicate(n_true / 2, rep(rnorm(nlev2, 0, 1), each = n_level_2)))
 
   ## JOIN FIXED EFFECTS TOGETHER ##
-  outcome <- as.data.frame(cbind(uncor_sd_true, fixed_effect_level_2))
+  outcome <- bind_cols(c(fixed_effect_level_1, fixed_effect_level_2))
+
   colnames(outcome) <- c(1:ncol(outcome))
   outcome <- outcome  %>%
     rename_with(~ paste0("true_", .))
@@ -45,8 +54,18 @@ simulate_data_re <- function(nrows, ncols, n_true = 2, amplitude = 2,
 
   ### THIS CODE CALCULATES A Y VALUE FOR EACH ROW FROM OUR 10 X VARIABLES WITH AN INTERCEPT = 1 AND TH EADDITION OF THE RANDON ERROR TERM
   ## SO THIS EFFECTIVELY PREDICTS THE Y FROM A MODEL USING OUR 10 VARIABLES AND A BETA=2 FOR EACH VARIABLE
-
-  outcome$y_out <- 0 + (outcome$true_1 * amplitude) + (outcome$true_2 * amplitude) + outcome$RE + outcome$error
+  # TODO: n_true == N
+  outcome$outcome <- 0 +
+    (outcome$true_1 * amplitude) +
+    (outcome$true_2 * amplitude) +
+    (outcome$true_3 * amplitude) +
+    (outcome$true_4 * amplitude) +
+    (outcome$true_5 * amplitude) +
+    (outcome$true_6 * amplitude) +
+    (outcome$true_7 * amplitude) +
+    (outcome$true_8 * amplitude) +
+    outcome$RE +
+    outcome$error
 
   outcome <- outcome %>% dplyr::select(-error, -RE)
 
@@ -59,12 +78,12 @@ simulate_data_re <- function(nrows, ncols, n_true = 2, amplitude = 2,
   outcome$level2 <- unlist(t(df_rand[-1]))
 
   outcome <- outcome %>%
-    select(y_out, level2, everything())
+    select(outcome, level2, everything())
 
   ## Add noise variables
   noise_vars <- replicate(num_noise_vars, rnorm(nrows, 0, 1)) %>%
     as_tibble() %>%
     rename_with(~ paste0("junk_", .x))
 
-  return(as.data.frame(cbind(outcome, noise_vars)))
+  return(as_tibble(cbind(outcome, noise_vars)))
 }
